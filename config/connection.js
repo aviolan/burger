cdc0fbe747a53748f6003ec388c8c6dfea976d87
Andrew Violan <andrew.violan@gmail.com>
2019-01-01 15:36:00 -0800

    modify code

--- config/connection.js
+++ config/connection.js
@@ -6,7 +6,7 @@ if (process.env.JAWSDB_URL) {
 } else {
     connection = mysql.createConnection({
         port: 3000,
-        user: 'root',
+        user: "root",
         password: "",
         database: "burgers_db"
     });
