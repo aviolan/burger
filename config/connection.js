3f97d12a14637a59d485ba436877534dc97d00b8
Andrew Violan <andrew.violan@gmail.com>
2019-01-02 18:13:50 -0800

    db initiated

--- config/connection.js
+++ config/connection.js
@@ -1,16 +1,11 @@
 var mysql = require("mysql");
-var connection;
-
-if (process.env.JAWSDB_URL) {
-    connnection = mysql.createConnection(process.env.JAWSDB_URL);
-} else {
-    connection = mysql.createConnection({
-        port: 3000,
-        user: "root",
-        password: "",
-        database: "burgers_db"
-    });
-};
+var connection = mysql.createConnection({
+    port: 3306,
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "burgers_db"
+});
 
 connection.connect(function(err) {
     if (err) {
